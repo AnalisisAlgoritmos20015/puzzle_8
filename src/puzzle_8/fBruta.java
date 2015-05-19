a899d9c39d6e375f4bda098f14e0e579166201b9
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2015-05-19 17:09:30 -0300

    Viendo las distintos casos

--- src/puzzle_8/fBruta.java
+++ src/puzzle_8/fBruta.java
@@ -6,6 +6,7 @@
 package puzzle_8;
 
 import java.util.Objects;
+import java.util.Random;
 
 /**
  *
@@ -77,30 +78,7 @@ public class fBruta {
     }
     
     //VEMOS LA DIRECCION
-    /*private int BuscarCoordenadai(){
-        int cordi = 0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(ini.getPos(i, j) == 0){
-                    cordi = i;
-                }
-            }
-        }
-        return cordi;
-    }
-    private int BuscarCoordenadaj(){
-        int cordj = 0;
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(ini.getPos(i, j) == 0){
-                    cordj = j;
-                }
-            }
-        }
-        return cordj;
-    }*/
-    
-    private coordenadas BuscarCoordenadai(){
+    private coordenadas BuscarCoordenada(){
         coordenadas  coord = new coordenadas(-1,-1);
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
@@ -111,6 +89,119 @@ public class fBruta {
             }
         }
         return coord;
+    }
+    
+    public void BuscarDireccion(){
+        Random  rnd = new Random();
+        coordenadas xy = new coordenadas(BuscarCoordenada());
+        if(xy.getI()%2 == 0 && xy.getJ()%2 == 0){ // EN EL CASO QUE ESTE EN LAS ESQUINAS
+            //EN CADA UNA DE LAS SIGUINTES IF SE PUEDEN TOMAR DOS CAMINOS
+            if(xy.getI() == 0 && xy.getJ() == 0){ // ESQUINA SUPERIOR IZQUIERDA
+                // BUSCAMOS EL G(N) POR LA ESQUINA SUPERIOR IZQUIERDA
+                if((int)(rnd.nextDouble() * 2 + 1) == 1){ // ABAJO
+                }
+                else{ // DERECHA
+                }
+            }
+            else {
+                if(xy.getI() == 2 && xy.getJ() == 0){ // ESQUINA INFERIOR IZQUIERDA
+                    // BUSCAMOS EL G(N) POR LA ESQUINA INFERIOR IZQUIERDA
+                    if((int)(rnd.nextDouble() * 2 + 1) == 1){ // ARRIBA
+                    }
+                    else{ // DERECHA
+                    }
+                }
+                else {
+                    if(xy.getI() == 0 && xy.getJ() == 2){ // ESQUINA SUPERIOR DERECHA
+                        // BUSCAMOS EL G(N) POR LA ESQUINA SUPERIOR DERECHA
+                        if((int)(rnd.nextDouble() * 2 + 1) == 1){ // ABAJO
+                        }
+                        else{ // IZQUIERDA
+                        }
+                    }
+                    else{
+                        if(xy.getI() == 2 && xy.getJ() == 2){ // ESQUINA INFERIOR DERECHA
+                            // BUSCAMOS EL G(N) POR LA ESQUINA INFERIOR DERECHA
+                            if((int)(rnd.nextDouble() * 2 + 1) == 1){ // ARRIBA
+                            }
+                            else{ // IZQUIERDA
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            if((xy.getI()+xy.getJ())%2 != 0){ // EN EL CASO QUE ESTE A LOS COSTADOS DEL CENTRO
+                //EN CADA UNA DE LAS SIGUINTES IF SE PUEDEN TOMAR TRES CAMINOS
+                if(xy.getI() == 0 && xy.getJ() == 1){ //ARRIBA
+                    // BUSCAMOS EL G(N) POR ARRIBA
+                    if((int)(rnd.nextDouble() * 3 + 1) == 1){ //IZQUIERDA
+                    }
+                    else{
+                        if((int)(rnd.nextDouble() * 3 + 1) == 2){ // DERECHA
+                        }
+                        else{ //ABAJO
+                        }
+                    }
+                }
+                else{
+                    if(xy.getI() == 2 && xy.getJ() == 1){ // ABAJO
+                    // BUSCAMOS EL G(N) POR ABAJO
+                        if((int)(rnd.nextDouble() * 3 + 1) == 1){ //IZQUIERDA
+                        }
+                        else{
+                            if((int)(rnd.nextDouble() * 3 + 1) == 2){ // DERECHA
+                            }
+                            else{ //ARRIBA
+                            }
+                        }
+                    }
+                    else{
+                        if(xy.getI() == 1 && xy.getJ() == 0){ //IZQUIERDA
+                            // BUSCAMOS EL G(N) POR IZQUIERDA
+                            if((int)(rnd.nextDouble() * 3 + 1) == 1){ //ARRIBA
+                            }
+                            else{
+                                if((int)(rnd.nextDouble() * 3 + 1) == 2){ // ABAJO
+                                }
+                                else{ // DERECHA
+                                }
+                            }   
+                        }
+                        else{
+                            if(xy.getI() == 1 && xy.getJ() == 2){ // DERECHA
+                                // BUSCAMOS EL G(N) POR DERECHA
+                                if((int)(rnd.nextDouble() * 3 + 1) == 1){ //ARRIBA
+                                }
+                                else{
+                                    if((int)(rnd.nextDouble() * 3 + 1) == 2){ // ABAJO
+                                    }
+                                    else{ // IZQUIERDA
+                                    }
+                                }  
+                            }
+                        }
+                    }
+                }
+            }
+            else{ // EN EL CASO QUE ESTE EN EL CENTRO
+                //EN ESTE CASO EXISTEN CUATRO CAMINOS DIFERENTES
+                if((int)(rnd.nextDouble() * 4 + 1) == 1){ // ARRIBA
+                }
+                else{
+                    if((int)(rnd.nextDouble() * 4 + 1) == 2){ // ABAJO
+                    }
+                    else{
+                        if((int)(rnd.nextDouble() * 4 + 1) == 3){ // IZQUIERDA
+                        }
+                        else{ // DERECHA
+                        }
+                    }
+                }
+                
+            }
+        }
     }
     
 }
